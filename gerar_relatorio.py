21a03ff7845a294044064712510e968d550c4da4
gui2609 <guilhermeschumann26@gmail.com>
2025-11-12 17:07:20 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- gerar_relatorio.py
+++ gerar_relatorio.py
@@ -14,13 +14,20 @@ from sklearn.linear_model import LinearRegression
 
 # ============================================================================
 # --- CONFIGURAÇÃO DO CLIENTE ---
-# ID e Estações do cliente "Dirceu Martins Comiran"
+# ID e Estações do cliente "Maracai"
 # ============================================================================
-CLIENTE_ID = 2271718 # ID DO CLIENTE (GROWER_ID)
-CLIENTE_NOME = "Dirceu Martins Comiran" # NOME DO CLIENTE
+CLIENTE_ID = 886395 # ID DO CLIENTE (GROWER_ID)
+CLIENTE_NOME = "Maracai" # NOME DO CLIENTE
 ESTACOES_DO_CLIENTE = [
-    {'name': 'Portal da Amazonia', 'id_estacao': '58972', 'latitude': -14.0032, 'longitude': -59.2614},
-    {'name': 'Portal da Amazonia2', 'id_estacao': '65907', 'latitude': -14.0104, 'longitude': -59.2108},
+    {'name': 'Continental T-18', 'id_estacao': '80670', 'latitude': -11.4759, 'longitude': -55.2041},
+    {'name': 'Continental T-05', 'id_estacao': '80682', 'latitude': -11.52, 'longitude': -55.2353},
+    {'name': 'Continental T-23', 'id_estacao': '81000', 'latitude': -11.4718, 'longitude': -55.2422},
+    {'name': 'Continental T-02', 'id_estacao': '80678', 'latitude': -11.5334, 'longitude': -55.2177},
+    {'name': 'Continental T-24', 'id_estacao': '80681', 'latitude': -11.4631, 'longitude': -55.2551},
+    {'name': 'Continental T-14', 'id_estacao': '80677', 'latitude': -11.4937, 'longitude': -55.2148},
+    {'name': 'PLV Continental', 'id_estacao': '80676', 'latitude': -11.4644, 'longitude': -55.2021},
+    {'name': 'Continental Tomazzi', 'id_estacao': '81001', 'latitude': -11.4796, 'longitude': -55.1567},
+    {'name': 'Continental T-08', 'id_estacao': '50977', 'latitude': -11.5123, 'longitude': -55.2166}
 ]
 # Define quantos anos de dados históricos buscar
 ANOS_DE_HISTORICO = 2
@@ -1049,4 +1056,5 @@ if __name__ == "__main__":
         # Faz o script falhar para que o GitHub Actions reporte o erro
 
         exit(1)
+
 
